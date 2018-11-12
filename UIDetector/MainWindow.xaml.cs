98a1cfcd8a24e91da1f232d1ba2006deb1d6babb
chiffon <chiffonqian@gmail.com>
2018-11-13 00:18:38 +0800

    add win32 file

--- UIDetector/MainWindow.xaml.cs
+++ UIDetector/MainWindow.xaml.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using 
+using Microsoft.Win32;
+using Automation = System.Windows.Automation;
+using System.Windows.Automation;
+using System.Diagnostics;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -13,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Win32Native;
 
 namespace UIDetector
 {
@@ -22,6 +26,10 @@ namespace UIDetector
     public partial class MainWindow : Window
     {
         MouseHook mouseHook = new MouseHook();
+        AutomationElement rootElement = AutomationElement.RootElement;
+        string processName = String.Empty;
+        string processPath = String.Empty;
+        string mainwndText = String.Empty;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +38,34 @@ namespace UIDetector
 
         private void MouseHook_LeftButtonUp(MouseHook.MSLLHOOKSTRUCT mouseStruct)
         {
+            mouseHook.Uninstall();
+            int hwnd = User.WindowFromPoint(mouseStruct.pt.x, mouseStruct.pt.y);
+            if(0 == hwnd)
+            {
+                return;
+            }
+            int root = User.GetAncestor((IntPtr)hwnd, User.GA_ROOT);
+
+            int pid=0;
+            User.GetWindowThreadProcessId((IntPtr)root,ref pid);
+
+            
+            
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process p in processlist)
+            {
+                if(p.Id == pid)
+                {
+                    mainwndText = p.MainWindowTitle;
+                    processName = p.ProcessName;
+                    processPath = p.MainModule.FileName;
+                }
+            }
+            if (String.IsNullOrEmpty(processPath))
+            {
+                return;
+            }
+            Automation.Condition condition = new PropertyCondition(AutomationElement.NameProperty, mainwndText);
 
             throw new NotImplementedException();
         }
